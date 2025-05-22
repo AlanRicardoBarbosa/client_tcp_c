817dfc06cf2230f3127263da72ebe7dcf8e325f7
AlanRicardoBarbosa <aalanricardoo@gmail.com>
2025-05-22 18:00:41 -0300

    Trying to resolve exit problem

--- client.c
+++ client.c
@@ -21,13 +21,20 @@ int main(){
 	inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
 
 	connect(sock, (struct serv_add *)&serv_addr, sizeof(serv_addr));
+	
+	char message[1024];
+	char buffer[1024];
 
-	char *hello = "Hello World!";
-	send(sock, hello, strlen(hello), 0);
-
-	char buffer [1024] = {0};
-	read(sock, buffer, 1024);
-	printf("Server: %s\n", buffer);
-
+	while (1) {
+		printf("Write your message: ");
+		fgets(message, 1024, stdin);
+		send(sock, message, strlen(message), 0);
+		if(strcmp(message, "exit\n") == 0) {
+			break;
+		}	
+		memset(buffer, 0, sizeof(buffer));
+		read(sock, buffer, 1024);
+		printf("Server: %s\n", buffer);
+}
 	return 0;
 }
